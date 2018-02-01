35beb039bf029d2dc6c975447f538b99e458d21c
asserebryanskiy <asserebryanskiy@gmail.com>
2018-02-01 17:35:21 +0300

    added scale button

--- src/main/java/controllers/GeneratorController.java
+++ src/main/java/controllers/GeneratorController.java
@@ -14,6 +14,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -34,6 +35,8 @@ public class GeneratorController {
     private static final int OFFSET_FROM_BORDER = 20;   // offset from nearest border (either vertical or horizontal)
     private static final String MUTE_SVG_PATH = "M5 17h-5v-10h5v10zm2-10v10l9 5v-20l-9 5zm15.324 4.993l1.646-1.659-1.324-1.324-1.651 1.67-1.665-1.648-1.316 1.318 1.67 1.657-1.65 1.669 1.318 1.317 1.658-1.672 1.666 1.653 1.324-1.325-1.676-1.656z";
     private static final String SOUND_SVG_PATH = "M5 17h-5v-10h5v10zm2-10v10l9 5v-20l-9 5zm17 4h-5v2h5v-2zm-1.584-6.232l-4.332 2.5 1 1.732 4.332-2.5-1-1.732zm1 12.732l-4.332-2.5-1 1.732 4.332 2.5 1-1.732z";
+    private static final String SCALE_SVG_PATH = "M6.426 10.668l-3.547-3.547-2.879 2.879v-10h10l-2.879 2.879 3.547 3.547-4.242 4.242zm11.148 2.664l3.547 3.547 2.879-2.879v10h-10l2.879-2.879-3.547-3.547 4.242-4.242zm-6.906 4.242l-3.547 3.547 2.879 2.879h-10v-10l2.879 2.879 3.547-3.547 4.242 4.242zm2.664-11.148l3.547-3.547-2.879-2.879h10v10l-2.879-2.879-3.547 3.547-4.242-4.242z";
+    private static final String UNSCALE_SVG_PATH = "M16.586 19.414l-2.586 2.586v-8h8l-2.586 2.586 4.586 4.586-2.828 2.828-4.586-4.586zm-13.758-19.414l-2.828 2.828 4.586 4.586-2.586 2.586h8v-8l-2.586 2.586-4.586-4.586zm16.586 7.414l2.586 2.586h-8v-8l2.586 2.586 4.586-4.586 2.828 2.828-4.586 4.586zm-19.414 13.758l2.828 2.828 4.586-4.586 2.586 2.586v-8h-8l2.586 2.586-4.586 4.586z";
     private static final double TEXT_HEIGHT_RATIO = 75d/190d;  // ratio of text height relative to video height
     private static final String MUSIC_SRC = "/media/fort-boyard-monety.mp3";
     private static final String VIDEO_SRC = "/media/moz.mp4";
@@ -42,13 +45,15 @@ public class GeneratorController {
 
     public StackPane root;
     public MediaView mediaView;
-    public SVGPath backBtn;
+    public Pane backBtn;
     public Label number;
     public Button startBtn;
     public SVGPath muteSvg;
     public HBox audioControls;
-
     public Slider volumeSlider;         // regulates music volume
+    public Pane fullScreenBtn;
+    public SVGPath fullScreenSvg;
+
     private MediaPlayer video;          // media player, when video is shown (completely bugged!)
     private Media media;                // media with video
     private boolean run;                // if an application now is in a run phase? (video is playing, generator is working)
@@ -64,16 +69,13 @@ public class GeneratorController {
     public void init(RandomGenerator generator, boolean disallowRepeats) {
 //        initLogo();
 
-        backBtn.setManaged(false);
-        audioControls.setManaged(false);
-        double height = audioControls.getBoundsInLocal().getHeight();
-        audioControls.layoutYProperty().bind(root.maxHeightProperty()
-                .subtract(OFFSET_FROM_BORDER + height + TOP_PANEL_HEIGHT));
-        audioControls.setLayoutX(OFFSET_FROM_BORDER);
+//        audioControls.setManaged(false);
         volumeSlider.setMax(1);
         volumeSlider.setValue(1);
         // to make available pressing space button to start/stop playing
         volumeSlider.setOnMouseReleased(e -> startBtn.requestFocus());
+
+        positionNodes();
 
         // set up video
         media = new Media(getClass().getResource(VIDEO_SRC).toExternalForm());
@@ -118,6 +120,20 @@ public class GeneratorController {
             play();
         });
 
+    }
+
+    private void positionNodes() {
+        double height = audioControls.getBoundsInLocal().getHeight();
+        audioControls.setMaxHeight(height);
+        // bind to lower left corner
+        audioControls.setTranslateX(OFFSET_FROM_BORDER);
+        audioControls.translateYProperty().bind(root.maxHeightProperty()
+                .subtract(OFFSET_FROM_BORDER + height + TOP_PANEL_HEIGHT));
+        backBtn.setTranslateX(OFFSET_FROM_BORDER);
+        backBtn.setTranslateY(OFFSET_FROM_BORDER);
+        fullScreenBtn.translateXProperty().bind(root.maxWidthProperty()
+                .subtract(OFFSET_FROM_BORDER + fullScreenBtn.getBoundsInLocal().getWidth()));
+        fullScreenBtn.setTranslateY(OFFSET_FROM_BORDER);
     }
 
     private void substituteNumberIfNeeded() {
@@ -172,7 +188,9 @@ public class GeneratorController {
 
             // change size of the text
             double newFontSize = window.getHeight() * TEXT_HEIGHT_RATIO;
-            number.setStyle("-fx-font-size:" + newFontSize);
+            System.out.println(window.getHeight());
+            System.out.println(newFontSize);
+            if (!Double.isNaN(newFontSize)) number.setStyle("-fx-font-size:" + newFontSize);
             number.setTranslateY(newFontSize * 0.15);   // because it runs into logo if not
         }
     }
@@ -243,6 +261,26 @@ public class GeneratorController {
 
     public void setSameNumber(String sameNumber) {
         this.alwaysSameNumber = sameNumber;
+    }
+
+    public void changeSvgColor(MouseEvent event) {
+        SVGPath svg = (SVGPath) ((Pane) event.getSource()).getChildren().get(0);
+        if (event.getEventType().equals(MouseEvent.MOUSE_ENTERED)) {
+            svg.setStyle("-fx-fill: white");
+        } else {
+            svg.setStyle("-fx-fill: gray-color-transparent");
+        }
+    }
+
+    public void handleSetFullScreen(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        if (stage.isFullScreen()) {
+            fullScreenSvg.setContent(SCALE_SVG_PATH);
+            stage.setFullScreen(false);
+        } else {
+            fullScreenSvg.setContent(UNSCALE_SVG_PATH);
+            stage.setFullScreen(true);
+        }
     }
 
     /*private void shutDownTimer() {
